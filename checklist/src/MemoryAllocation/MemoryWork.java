692bc7f813411bebfa6dd51738904f67f268d981
Comptechcare <Comptechcare@DESKTOP-VQV1SHB>
2022-04-23 15:47:57 +0530

    test

--- checklist/src/MemoryAllocation/MemoryWork.java
+++ checklist/src/MemoryAllocation/MemoryWork.java
@@ -2,10 +2,10 @@ package MemoryAllocation;
 
 public class MemoryWork {
 	public static void main(String[] args) {
-	
-		var textBox1 = new TextBox();
-		var textBox2 = textBox1;
-		textBox2.setText("Memory Allocation Work");
-		System.out.println(textBox1.text);
+//		var textBox2 = textBox1;
+//		textBox2.setText("Memory Allocation Work");
+//		System.out.println(textBox1.text);
+//	}
+
 	}
 }
